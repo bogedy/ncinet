174e4312f516d4354f516279fd43c006b8bdc799
aschankler <aschankler@haverford.edu>
2018-03-05 23:22:02 -0500

    Update paths.

--- ncinet/__init__.py
+++ ncinet/__init__.py
@@ -4,10 +4,9 @@ Module to build and train neural networks to predict protein properties.
 
 __version__ = "0.3.0"
 
-import os
 
 # Directory where summaries and checkpoints are written.
 #WORK_DIR = '/work/05187/ams13/maverick/Working/TensorFlow/ncinet'
 WORK_DIR = 'C:/Users/schan/Documents/TF_run'
 
-FINGERPRINT_DIR = '/work/projects/SD2E-Community/prod/data/shared-q0-hackathon/team10/nci_rocklin'
+FINGERPRINT_DIR = '/work/projects/SD2E-Community/prod/data/archive/shared-q0-hackathon/team_protstab/nci_rocklin'

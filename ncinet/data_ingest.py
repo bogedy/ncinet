75cc90ae813886a00e4dd3b1f936602e38de0a1b
aschankler <aschankler@haverford.edu>
2018-05-29 09:43:27 -0400

    Load pre-made splits.

--- ncinet/data_ingest.py
+++ ncinet/data_ingest.py
@@ -325,8 +325,23 @@ def load_data_from_raws(config):
             # Save data to file
             np.savez(os.path.join(archive_dir, config.full_archive_name), **full_data)
         elif type(config.score_path) is tuple:
-            # Todo: logic for premade splits
-            pass
+            assert len(config.score_path) == 2
+
+            # Load train and test data
+            train_path, test_path = config.score_path
+            train_data = load_data_from_tables(train_path, config.nci_dir)
+            test_data = load_data_from_tables(test_path, config.nci_dir)
+
+            # Convert topologies to indices
+            topo_index = index_topologies(train_data['topologies'], test_data['topologies'])
+            write_topo_labels(os.path.join(config.archive_dir, config.topo_index_name), topo_index)
+            train_data['topologies'] = apply_topo_index(train_data['topologies'], topo_index)
+            test_data['topologies'] = apply_topo_index(test_data['topologies'], topo_index)
+
+            # Save data to files
+            name_fstring = "{prefix}_{{batch}}.npz".format(prefix=config.archive_prefix)
+            np.savez(os.path.join(config.archive_dir, name_fstring.format(batch=config.tt_tags[0])), **train_data)
+            np.savez(os.path.join(config.archive_dir, name_fstring.format(batch=config.tt_tags[1])), **test_data)
         else:
             raise ValueError("Unexpected value for 'score_path'")
     else:
